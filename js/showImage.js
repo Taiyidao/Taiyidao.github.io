438a69fbc5664929087953a15a0ff9e947c94f75
Lixiney <3034246613@qq.com>
2025-05-21 23:24:38 +0800

    Site updated: 2025-05-21 23:24:37

--- js/showImage.js
+++ js/showImage.js
@@ -1,7 +1,7 @@
 function showImage() {
     Swal.fire({
       text: "扫描二维码联系",
-      imageUrl: "http://localhost:4000/img/code.png",
+      imageUrl: "/img/code.png",
       imageWidth: 400,
       imageHeight: 400,
       imageAlt: "扫描二维码添加微信",
